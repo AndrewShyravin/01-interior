609fd2f4414d2eff50e453e3a52ed2c715eddf59
Andrey Shyravin <andycrab1997@gmail.com>
2024-05-13 23:24:21 +0400

    Переименовал папку menu в header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
-import Header from '../components/Menu/Header';
+import Header from '../components/Header/Header';
 
 const MainLayout = () => {
   return (
